46cb3e07eb3e7386470bc5653d2b81d0b056c5a8
Shrikrishna Singh <krishnasingh.ss30@gmail.com>
2024-02-22 14:05:34 +0530

    Add Annotations permission

--- label_studio/tasks/api.py
+++ label_studio/tasks/api.py
@@ -2,6 +2,8 @@
 """
 import logging
 
+import drf_yasg.openapi as openapi
+from core.api_permissions import AnnotationsPermission
 from core.feature_flags import flag_set
 from core.mixins import GetParentObjectMixin
 from core.permissions import ViewClassPermission, all_permissions
@@ -22,6 +24,7 @@ from projects.functions.stream_history import fill_history_annotation
 from projects.models import Project
 from rest_framework import generics, viewsets
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework.response import Response
 from tasks.models import Annotation, AnnotationDraft, Prediction, Task
@@ -450,7 +453,7 @@ class AnnotationAPI(generics.RetrieveUpdateDestroyAPIView):
         PATCH=all_permissions.annotations_change,
         DELETE=all_permissions.annotations_delete,
     )
-
+    permission_classes = (IsAuthenticated, AnnotationsPermission)
     serializer_class = AnnotationSerializer
     queryset = Annotation.objects.all()
 
@@ -567,6 +570,7 @@ class AnnotationsListAPI(GetParentObjectMixin, generics.ListCreateAPIView):
         GET=all_permissions.annotations_view,
         POST=all_permissions.annotations_create,
     )
+    permission_classes = (IsAuthenticated, AnnotationsPermission)
     parent_queryset = Task.objects.all()
 
     serializer_class = AnnotationSerializer
@@ -665,6 +669,7 @@ class AnnotationDraftListAPI(generics.ListCreateAPIView):
         GET=all_permissions.annotations_view,
         POST=all_permissions.annotations_create,
     )
+    permission_classes = (IsAuthenticated, AnnotationsPermission)
     queryset = AnnotationDraft.objects.all()
 
     def filter_queryset(self, queryset):
@@ -690,6 +695,8 @@ class AnnotationDraftAPI(generics.RetrieveUpdateDestroyAPIView):
         PATCH=all_permissions.annotations_change,
         DELETE=all_permissions.annotations_delete,
     )
+    permission_classes = (IsAuthenticated, AnnotationsPermission)
+    swagger_schema = None
 
 
 @method_decorator(
@@ -876,6 +883,7 @@ class PredictionAPI(viewsets.ModelViewSet):
 class AnnotationConvertAPI(generics.RetrieveAPIView):
     permission_required = ViewClassPermission(POST=all_permissions.annotations_change)
     queryset = Annotation.objects.all()
+    permission_classes = (IsAuthenticated, AnnotationsPermission)
 
     def process_intermediate_state(self, annotation, draft):
         pass
