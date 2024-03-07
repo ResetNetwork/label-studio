c29ed78af68f5c235281693f9d7e55c1742956ec
Shrikrishna Singh <krishnasingh.ss30@gmail.com>
2024-03-07 17:55:42 +0530

    Update Annotation.js

--- web/libs/editor/src/components/App/Annotation.js
+++ web/libs/editor/src/components/App/Annotation.js
@@ -9,7 +9,6 @@ export function Annotation({ annotation, root }) {
         annotation.resetReady();
       }
     };
-  }, [annotation?.pk, annotation?.id]);
-
+  }, [annotation.pk, annotation.id]);
   return root ? Tree.renderItem(root, annotation) : null;
 }
