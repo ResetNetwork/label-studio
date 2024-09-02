186b9f7f7870c67b836d0e15507466c7f495e60b
fredheir <fredheir@gmail.com>
2024-09-02 19:55:06 +0300

    Update api.py
    maybe better to keep destroy

--- label_studio/data_manager/api.py
+++ label_studio/data_manager/api.py
@@ -167,6 +167,8 @@ class ViewAPI(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
 
     @extend_schema(
         tags=['Data Manager'],
