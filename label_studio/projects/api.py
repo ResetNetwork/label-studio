53309ca4ff43427743ffca5c1516a4ff92aecb38
fredheir <fredheir@gmail.com>
2025-01-12 23:18:56 +0200

    Add 2 more metrics

--- label_studio/projects/api.py
+++ label_studio/projects/api.py
@@ -1073,10 +1073,10 @@ class UserMetricsAPI(generics.RetrieveAPIView):
                 'annotations_today': counts['today'],
                 'annotations_week': counts['week'],
                 'annotations_quarter': counts['quarter'],
+                'total_time_week': total_hours,
                 'avg_annotation_time': avg_time,
                 'regularity': int((days_with_annotations / 10) * 100),
                 'projects_contributed': counts['projects_contributed'],
-                'total_time_week': total_hours
             }
 
             # Cache the results
